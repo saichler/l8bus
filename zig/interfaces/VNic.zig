4dd17d8b07a3a8fa9482a47ecc8ded2b0144ca98
Sharon Aicler <saichler@cisco.com>
2025-02-18 10:52:28 -0800

    Start to learn zig

---
+++ zig/interfaces/VNic.zig
@@ -0,0 +1,9 @@
+const VNic = struct {
+    this:*VNic,
+    pub fn Start(this: *VNic) !void {
+        return this.Start(this);
+    }
+    pub fn Shutdown(this: *VNic) !void {
+        return this.Shutdown(this);
+    }
+};
