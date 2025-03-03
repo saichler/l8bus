831cb547d6cc6deffd0e731fedb1b18220cff1d7
Sharon Aicler <saichler@cisco.com>
2025-03-02 18:34:28 -0800

    Finish Transaction Tests

--- go/tests/TestTransaction_test.go
+++ go/tests/TestTransaction_test.go
@@ -5,7 +5,6 @@ import (
 	"github.com/saichler/shared/go/tests"
 	"github.com/saichler/shared/go/types"
 	"testing"
-	"time"
 )
 
 func TestTransaction(t *testing.T) {
@@ -19,19 +18,37 @@ func TestTransaction(t *testing.T) {
 		}
 	}()
 	pb := &tests.TestProto{MyString: "test"}
-	_, err := eg3.Request(types.CastMode_Single, types.Action_POST, 0, "TestProto", pb)
+	resp, err := eg3.Request(types.CastMode_Single, types.Action_POST, 0, "TestProto", pb)
 	if err != nil {
 		log.Fail(t, err.Error())
 		return
 	}
-	_, err = eg3.Request(types.CastMode_Single, types.Action_POST, 0, "TestProto", pb)
+	tr := resp.(*types.Transaction)
+	if tr.State != types.TransactionState_Finished {
+		log.Fail(t, "transaction state is not finished")
+	}
+
+	resp, err = eg3.Request(types.CastMode_Single, types.Action_POST, 0, "TestProto", pb)
 	if err != nil {
 		log.Fail(t, err.Error())
 		return
 	}
-	time.Sleep(1 * time.Second)
+	tr = resp.(*types.Transaction)
+	if tr.State != types.TransactionState_Finished {
+		log.Fail(t, "transaction state is not finished")
+	}
+
+	if tsps["eg1"].PostNumber != 2 {
+		log.Fail(t, "Expected post to be 2 but it is ", tsps["eg1"].PostNumber)
+	}
 	if tsps["eg2"].PostNumber != 2 {
 		log.Fail(t, "Expected post to be 2 but it is ", tsps["eg2"].PostNumber)
+	}
+	if tsps["eg3"].PostNumber != 2 {
+		log.Fail(t, "Expected post to be 2 but it is ", tsps["eg3"].PostNumber)
+	}
+	if tsps["eg4"].PostNumber != 2 {
+		log.Fail(t, "Expected post to be 2 but it is ", tsps["eg4"].PostNumber)
 	}
 }
 
