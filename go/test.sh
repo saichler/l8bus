9fe6c1570137fc3e0f3181bbc93c1790120cba70
Sharon Aicler <saichler@gmail.com>
2025-04-01 09:42:11 -0700

    remove go.mod

--- go/test.sh
+++ go/test.sh
@@ -3,12 +3,12 @@
 # Fail on errors and don't open cover file
 set -e
 # clean up
-git checkout go.mod
+rm -rf go.mod
 rm -rf go.sum
 rm -rf vendor
 
 # fetch dependencies
-#cp go.mod.main go.mod
+go mod init
 GOPROXY=direct GOPRIVATE=github.com go mod tidy
 ./build-security.sh
 go mod vendor
