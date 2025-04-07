f95323c28a0f3f03de7363ef810c10543f558ab1
Sharon Aicler <saichler@gmail.com>
2025-04-07 05:00:40 -0700

    Improve serialization

--- go/overlay/vnic/RX.go
+++ go/overlay/vnic/RX.go
@@ -21,36 +21,36 @@ func newRX(vnic *VirtualNetworkInterface) *RX {
 	return rx
 }
 
-func (rx *RX) start() {
-	go rx.readFromSocket()
-	go rx.notifyRawDataListener()
+func (this *RX) start() {
+	go this.readFromSocket()
+	go this.notifyRawDataListener()
 }
 
-func (rx *RX) shutdown() {
-	rx.shuttingDown = true
-	if rx.vnic.conn != nil {
-		rx.vnic.conn.Close()
+func (this *RX) shutdown() {
+	this.shuttingDown = true
+	if this.vnic.conn != nil {
+		this.vnic.conn.Close()
 	}
-	rx.rx.Shutdown()
+	this.rx.Shutdown()
 }
 
-func (rx *RX) name() string {
+func (this *RX) name() string {
 	return "RX"
 }
 
 // loop and Read incoming data from the socket
-func (rx *RX) readFromSocket() {
+func (this *RX) readFromSocket() {
 	// While the port is active
-	for rx.vnic.running {
+	for this.vnic.running {
 		// read data ([]byte) from socket
-		data, err := nets.Read(rx.vnic.conn, rx.vnic.resources.SysConfig())
+		data, err := nets.Read(this.vnic.conn, this.vnic.resources.SysConfig())
 		//If therer is an error
 		if err != nil {
-			if rx.vnic.IsVNet {
+			if this.vnic.IsVNet {
 				break
 			}
-			if !rx.shuttingDown {
-				rx.vnic.reconnect()
+			if !this.shuttingDown {
+				this.vnic.reconnect()
 				continue
 			} else {
 				break
@@ -58,8 +58,8 @@ func (rx *RX) readFromSocket() {
 		}
 		if data != nil {
 			// If still active, write the data to the RX queue
-			if rx.vnic.running {
-				rx.rx.Add(data)
+			if this.vnic.running {
+				this.rx.Add(data)
 			}
 		} else {
 			// If data is nil, it means the port was shutdown
@@ -67,43 +67,42 @@ func (rx *RX) readFromSocket() {
 			break
 		}
 	}
-	rx.vnic.resources.Logger().Debug("RX for ", rx.vnic.name, " ended.")
+	this.vnic.resources.Logger().Debug("RX for ", this.vnic.name, " ended.")
 	//Just in case, mark the port as shutdown so other thread will stop as well.
-	rx.vnic.Shutdown()
+	this.vnic.Shutdown()
 }
 
 // Notify the RawDataListener on new data
-func (rx *RX) notifyRawDataListener() {
+func (this *RX) notifyRawDataListener() {
 	// While the port is active
-	for rx.vnic.running {
+	for this.vnic.running {
 		// Read next data ([]byte) block
-		data := rx.rx.Next()
+		data := this.rx.Next()
 		// If data is not nil
 		if data != nil {
-			rx.vnic.stats.RxMsgCount++
-			rx.vnic.stats.RxDataCont += int64(len(data))
+			this.vnic.stats.RxMsgCount++
+			this.vnic.stats.RxDataCont += int64(len(data))
 			// if there is a dataListener, this is a switch
-			if rx.vnic.resources.DataListener() != nil {
-				//rx.runRawHandleMessage(data)
-				rx.vnic.resources.DataListener().HandleData(data, rx.vnic)
+			if this.vnic.resources.DataListener() != nil {
+				this.vnic.resources.DataListener().HandleData(data, this.vnic)
 			} else {
-				msg, err := rx.vnic.protocol.MessageOf(data)
+				msg, err := this.vnic.protocol.MessageOf(data)
 				if err != nil {
-					rx.vnic.resources.Logger().Error(err)
+					this.vnic.resources.Logger().Error(err)
 					continue
 				}
-				pb, err := rx.vnic.protocol.ElementsOf(msg)
+				pb, err := this.vnic.protocol.ElementsOf(msg)
 				if err != nil {
-					rx.vnic.resources.Logger().Error(err)
+					this.vnic.resources.Logger().Error(err)
 					if msg.Request() {
 						resp := object.NewError(err.Error())
-						err = rx.vnic.Reply(msg, resp)
+						err = this.vnic.Reply(msg, resp)
 						if err != nil {
-							rx.vnic.resources.Logger().Error(err)
+							this.vnic.resources.Logger().Error(err)
 						}
 					} else if msg.Reply() {
 						resp := object.NewError(err.Error())
-						request := rx.vnic.requests.getRequest(msg.Sequence(), rx.vnic.resources.SysConfig().LocalUuid)
+						request := this.vnic.requests.getRequest(msg.Sequence(), this.vnic.resources.SysConfig().LocalUuid)
 						request.response = resp
 						request.cond.Broadcast()
 					}
@@ -113,7 +112,7 @@ func (rx *RX) notifyRawDataListener() {
 				//This is a reply message, should not find a handler
 				//and just notify
 				if msg.Reply() {
-					request := rx.vnic.requests.getRequest(msg.Sequence(), rx.vnic.resources.SysConfig().LocalUuid)
+					request := this.vnic.requests.getRequest(msg.Sequence(), this.vnic.resources.SysConfig().LocalUuid)
 					request.response = pb
 					request.cond.Broadcast()
 					continue
@@ -121,37 +120,37 @@ func (rx *RX) notifyRawDataListener() {
 				// Otherwise call the handler per the action & the type
 				// If Reauest == blocking, hence run in a go routing.
 				if msg.Request() {
-					go rx.handleMessage(msg, pb)
+					go this.handleMessage(msg, pb)
 				} else {
-					rx.handleMessage(msg, pb)
+					this.handleMessage(msg, pb)
 				}
 			}
 		}
 	}
-	rx.vnic.resources.Logger().Debug("ND for ", rx.vnic.name, " has Ended")
-	rx.vnic.Shutdown()
+	this.vnic.resources.Logger().Debug("ND for ", this.vnic.name, " has Ended")
+	this.vnic.Shutdown()
 }
 
-func (rx *RX) handleMessage(msg common.IMessage, pb common.IElements) {
+func (this *RX) handleMessage(msg common.IMessage, pb common.IElements) {
 	if msg.Action() == common.Reply {
-		request := rx.vnic.requests.getRequest(msg.Sequence(), rx.vnic.resources.SysConfig().LocalUuid)
+		request := this.vnic.requests.getRequest(msg.Sequence(), this.vnic.resources.SysConfig().LocalUuid)
 		request.response = pb
 		request.cond.Broadcast()
 	} else if msg.Action() == common.Notify {
-		resp := rx.vnic.resources.ServicePoints().Notify(pb, rx.vnic, msg, false)
+		resp := this.vnic.resources.ServicePoints().Notify(pb, this.vnic, msg, false)
 		if resp != nil && resp.Error() != nil {
-			rx.vnic.resources.Logger().Error(resp.Error())
+			this.vnic.resources.Logger().Error(resp.Error())
 		}
 	} else {
 		//Add bool
-		resp := rx.vnic.resources.ServicePoints().Handle(pb, msg.Action(), rx.vnic, msg, false)
+		resp := this.vnic.resources.ServicePoints().Handle(pb, msg.Action(), this.vnic, msg, false)
 		if resp != nil && resp.Error() != nil {
-			rx.vnic.resources.Logger().Error(resp.Error())
+			this.vnic.resources.Logger().Error(resp.Error())
 		}
 		if msg.Request() {
-			err := rx.vnic.Reply(msg, resp)
+			err := this.vnic.Reply(msg, resp)
 			if err != nil {
-				rx.vnic.resources.Logger().Error(err)
+				this.vnic.resources.Logger().Error(err)
 			}
 		}
 	}
