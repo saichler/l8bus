867bdd3c7be9096879d1dff5c2b1db2791d76fff
Sharon Aicler <saichler@gmail.com>
2025-08-14 05:52:55 -0700

    Refactor send algo

--- go/tests/TestLeader_test.go
+++ go/tests/TestLeader_test.go
@@ -3,10 +3,10 @@ package tests
 import (
 	. "github.com/saichler/l8test/go/infra/t_resources"
 	. "github.com/saichler/l8test/go/infra/t_service"
-	. "github.com/saichler/l8test/go/infra/t_topology"
 	"github.com/saichler/l8types/go/ifs"
 	"github.com/saichler/layer8/go/overlay/health"
 	"testing"
+	"time"
 )
 
 func getLeader(uuid string) ifs.IVNic {
@@ -22,15 +22,14 @@ func getLeader(uuid string) ifs.IVNic {
 func TestLeader(t *testing.T) {
 	eg2_3 := topo.VnicByVnetNum(2, 3)
 	hc := health.Health(eg2_3.Resources())
-	leaderBefore := hc.Leader(ServiceName, 0)
+	leaderBefore := hc.LeaderFor(ServiceName, 0)
 	leader := getLeader(leaderBefore)
 	leader.Shutdown()
 	defer func() {
 		topo.RenewVnic(leader.Resources().SysConfig().LocalAlias)
 	}()
-	Sleep()
-	Sleep()
-	leaderAfter := hc.Leader(ServiceName, 0)
+	time.Sleep(time.Second * 10)
+	leaderAfter := hc.LeaderFor(ServiceName, 0)
 	if leaderAfter == leaderBefore {
 		Log.Fail(t, "Expected leader to change")
 		return
