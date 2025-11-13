a45778adfaa137b3aef6d4442b3e3a4c095ff8cf
Sharon Aicler <saichler@gmail.com>
2025-11-13 07:49:47 -0800

    add logs

--- go/overlay/vnic/KeepAlive.go
+++ go/overlay/vnic/KeepAlive.go
@@ -1,6 +1,7 @@
 package vnic
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 	"strconv"
@@ -74,6 +75,7 @@ func (this *KeepAlive) sendState() {
 	//this.vnic.resources.Logger().Debug("Sending Keep Alive for ", this.vnic.resources.SysConfig().LocalUuid, " ", this.vnic.resources.SysConfig().LocalAlias)
 	//We unicast to the vnet, it will multicast the change to all
 	this.vnic.Unicast(this.vnic.resources.SysConfig().RemoteUuid, health.ServiceName, health.ServiceArea(this.vnic.resources), ifs.PATCH, hp)
+	fmt.Println("Sending Keep Alive")
 }
 
 func memoryUsage() uint64 {
