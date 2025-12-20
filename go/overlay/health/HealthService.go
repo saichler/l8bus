6597218edb0090f590ef83dfc6df7832cc603db7
Sharon Aicler <saichler@gmail.com>
2025-12-20 08:58:45 -0800

    Add always overwrite for services

--- go/overlay/health/HealthService.go
+++ go/overlay/health/HealthService.go
@@ -1,7 +1,6 @@
 package health
 
 import (
-	"fmt"
 	"github.com/saichler/l8services/go/services/base"
 	"github.com/saichler/l8srlz/go/serialize/object"
 	"github.com/saichler/l8types/go/ifs"
@@ -33,6 +32,7 @@ func Activate(vnic ifs.IVNic, voter bool) {
 	serviceConfig.SetVoter(voter)
 	serviceConfig.SetTransactional(false)
 	serviceConfig.SetPrimaryKeys("AUuid")
+	serviceConfig.SetAlwaysOverwrite("lhHealth.services.servicetoareas")
 
 	webService := web.New(ServiceName, serviceArea, vnic.Resources().SysConfig().VnetPort)
 	webService.AddEndpoint(&l8api.L8Query{}, ifs.GET, &l8health.L8HealthList{})
@@ -83,26 +83,4 @@ func ServiceAreaByConfig(config *l8sysconfig.L8SysConfig) byte {
 		return byte(1)
 	}
 	return byte(0)
-}
-
-func Participants(serviceName string, serviceArea byte, r ifs.IResources) map[string]bool {
-	hc, _ := HealthServiceCache(r)
-	all := hc.All()
-	result := make(map[string]bool)
-	for _, h := range all {
-		hp := h.(*l8health.L8Health)
-		if hp.Services != nil && hp.Services.ServiceToAreas != nil {
-			fmt.Println("[Round Robin] ServiceName ", serviceName)
-			areas, ok := hp.Services.ServiceToAreas[serviceName]
-			if ok && areas.Areas != nil {
-				v, ok2 := areas.Areas[int32(serviceArea)]
-				fmt.Println("[Round Robin] ServiceArea ", serviceArea, " ", v, " ", ok2)
-				if ok2 {
-					result[hp.AUuid] = true
-				}
-			}
-		}
-	}
-	fmt.Println("[Round Robin] UUIDs ", len(result))
-	return result
 }
--- go/overlay/health/RoundRobin.go
+++ go/overlay/health/RoundRobin.go
@@ -1,6 +1,10 @@
 package health
 
-import "github.com/saichler/l8types/go/ifs"
+import (
+	"fmt"
+	"github.com/saichler/l8types/go/ifs"
+	"github.com/saichler/l8types/go/types/l8health"
+)
 
 type RoundRobin struct {
 	participants []string
@@ -24,4 +28,26 @@ func (this *RoundRobin) Next() string {
 	next := this.participants[this.index]
 	this.index++
 	return next
+}
+
+func Participants(serviceName string, serviceArea byte, r ifs.IResources) map[string]bool {
+	hc, _ := HealthServiceCache(r)
+	all := hc.All()
+	result := make(map[string]bool)
+	for _, h := range all {
+		hp := h.(*l8health.L8Health)
+		if hp.Services != nil && hp.Services.ServiceToAreas != nil {
+			fmt.Println("[Round Robin] ServiceName ", serviceName)
+			areas, ok := hp.Services.ServiceToAreas[serviceName]
+			if ok && areas.Areas != nil {
+				v, ok2 := areas.Areas[int32(serviceArea)]
+				fmt.Println("[Round Robin] ServiceArea ", serviceArea, " ", v, " ", ok2)
+				if ok2 {
+					result[hp.AUuid] = true
+				}
+			}
+		}
+	}
+	fmt.Println("[Round Robin] UUIDs ", len(result))
+	return result
 }
